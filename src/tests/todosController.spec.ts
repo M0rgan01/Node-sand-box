5489f4c9de0a73ddd5abe9e0a1235cb96a85e399
morgan <pichat.morgan@gmail.com>
2022-04-02 15:09:50 +0200

    Application server launch fix for tests

--- src/tests/todosController.spec.ts
+++ src/tests/todosController.spec.ts
@@ -2,32 +2,26 @@ import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 // mock need to be imported before app
 import { mock } from './KeycloakMock.spec';
-import app from '../app';
+import { createServer } from '../app';
 
 chai.use(chaiHttp);
 
-describe('authentication tests', () => {
-  before((done) => {
-    app.on('ready', () => {
-      done();
-    });
-  });
-
+describe('todos controller tests', () => {
   after(() => {
     mock.restore();
   });
 
-  it('Get todos should return all database todos', (done) => {
+  it('Get todos should return all database todos', async () => {
+    // given
+    const server = await createServer();
+
     // when
-    chai
-      .request(app)
+    const res = await chai
+      .request(server)
       .get('/todoAPI/todos')
-      .set('Authorization', 'secret')
-      .end((err, res) => {
-        // then
-        expect(res.status).eql(200);
-        expect(res.body).to.have.lengthOf(5);
-        done();
-      });
+      .set('Authorization', 'secret');
+
+    expect(res.status).eql(200);
+    expect(res.body).to.have.lengthOf(5);
   });
 });
